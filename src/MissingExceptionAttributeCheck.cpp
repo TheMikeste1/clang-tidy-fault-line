76f4e19ffd2147e9379ce22aa42556fe4add4e44
Michael Hegerhorst <michael.hegerhorst@gmail.com>
2026-07-30 22:04:50 -0600

    Missing attribute points towards throw and fixes declaration

--- src/MissingExceptionAttributeCheck.cpp
+++ src/MissingExceptionAttributeCheck.cpp
@@ -5,12 +5,14 @@
 #include <clang/AST/Attr.h>
 #include <clang/AST/Attrs.inc>
 #include <clang/AST/Decl.h>
+#include <clang/AST/DeclCXX.h>
 #include <clang/AST/Expr.h>
 #include <clang/AST/ExprCXX.h>
 #include <clang/Basic/AttrKinds.h>
 #include <clang/Basic/Diagnostic.h>
 #include <clang/Basic/LLVM.h>
 #include <llvm/ADT/StringRef.h>
+#include <llvm/Support/Casting.h>
 
 using namespace clang::ast_matchers;
 
@@ -19,32 +21,63 @@ namespace clang::tidy::fault_line {
 void MissingExceptionAttributeCheck::registerMatchers(MatchFinder *Finder) {
   // clang-format off
   Finder->addMatcher(
-    cxxThrowExpr(
-      unless(hasAncestor(compoundStmt(hasParent(cxxTryStmt()))))
-    ).bind("throwExpr"),
+    functionDecl(
+      hasDescendant(
+        cxxThrowExpr(
+          // TODO: Only match if the catch statement catches the throw type
+          unless(hasAncestor(compoundStmt(hasParent(cxxTryStmt()))))
+        ).bind("throw_expr")
+      )
+    ).bind("func"),
   this);
   // clang-format on
 }
 
 void MissingExceptionAttributeCheck::check(const MatchFinder::MatchResult &Result) {
-  const auto *funcDecl = Result.Nodes.getNodeAs<FunctionDecl>("func");
-  if (funcDecl == nullptr) {
+  const auto *FuncDefinition = Result.Nodes.getNodeAs<FunctionDecl>("func");
+  const auto *ThrowExpr = Result.Nodes.getNodeAs<CXXThrowExpr>("throw_expr");
+
+  if (FuncDefinition == nullptr || ThrowExpr == nullptr) {
     return;
   }
 
-  const bool hasExceptionAnnotation = std::any_of(funcDecl->attrs().begin(), funcDecl->attrs().end(), [](const Attr *const attr) {
-    const auto *const annotate = dyn_cast<AnnotateAttr>(attr);
-    if (annotate == nullptr) {
+  // We want to check the function declaration for the attributes.
+  const FunctionDecl *FuncDecl = FuncDefinition->getCanonicalDecl();
+  if (FuncDecl == nullptr) {
+    return;
+  }
+
+  const bool hasExceptionAnnotation = std::any_of(FuncDecl->attrs().begin(), FuncDecl->attrs().end(), [](const Attr *const Attr) {
+    const auto *const Annotate = dyn_cast<AnnotateAttr>(Attr);
+    if (Annotate == nullptr) {
       return false;
     }
 
-    const llvm::StringRef annotationStr = annotate->getAnnotation();
+    const llvm::StringRef annotationStr = Annotate->getAnnotation();
     return annotationStr.contains("throws_exception");
   });
 
-  if (!hasExceptionAnnotation) {
-    diag(funcDecl->getLocation(), "Function %0 can throw an exception but is not annotated as such.") << funcDecl << FixItHint::CreateInsertion(funcDecl->getBeginLoc(), "[[clang::annotate(\"throws_exception\")]] ");
+  if (hasExceptionAnnotation) {
+    return;
   }
+
+  const char *pFunctionType = "Function";
+  if (llvm::isa<CXXConstructorDecl>(FuncDefinition)) {
+    pFunctionType = "Constructor";
+  } else if (llvm::isa<CXXDestructorDecl>(FuncDefinition)) {
+    pFunctionType = "Destructor";
+  } else if (const auto *MethodDecl = llvm::dyn_cast<CXXMethodDecl>(FuncDecl)) {
+    if (MethodDecl->getParent()->isLambda()) {
+      // TODO: Figure out how to fix lambdas
+      // TODO: If this is an IIFE, we don't need to annotate. The caller should though.
+      diag(ThrowExpr->getThrowLoc(), "Lambda can throw an exception but is not annotated as such.") << FuncDecl;
+      return;
+    }
+
+    pFunctionType = "Method";
+  }
+
+  diag(ThrowExpr->getThrowLoc(), "%0 %1 can throw an exception but is not annotated as such.") << pFunctionType << FuncDecl << FixItHint::CreateInsertion(FuncDecl->getBeginLoc(), "[[clang::annotate(\"throws_exception\")]] ");
 }
 
 } // namespace clang::tidy::fault_line
--- test/test_missing_exception_attribute.cpp
+++ test/test_missing_exception_attribute.cpp
@@ -1,33 +1,41 @@
 // Test case for fault-line-missing-exception-attribute check
 
-// CHECK-MESSAGES: :[[@LINE+2]]:6: warning: Function 'missingExceptionAttribute' can throw an exception but is not annotated as such. [fault-line-missing-exception-attribute]
+// CHECK-MESSAGES: :[[@LINE+2]]:36: warning: Function 'missingExceptionAttribute' can throw an exception but is not annotated as such. [fault-line-missing-exception-attribute]
 // CHECK-FIXES: {{\[\[}}clang::annotate("throws_exception"){{\]\]}} void missingExceptionAttribute() { throw "Missing!"; }
 void missingExceptionAttribute() { throw "Missing!"; }
 
-// CHECK-MESSAGES: :[[@LINE+2]]:6: warning: Function 'missingExceptionAttributeInCatch' can throw an exception but is not annotated as such. [fault-line-missing-exception-attribute]
+// CHECK-MESSAGES: :[[@LINE+3]]:46: warning: Function 'missingExceptionAttributeDefinition' can throw an exception but is not annotated as such. [fault-line-missing-exception-attribute]
+// CHECK-FIXES: {{\[\[}}clang::annotate("throws_exception"){{\]\]}} void missingExceptionAttributeDefinition();
+void missingExceptionAttributeDefinition();
+void missingExceptionAttributeDefinition() { throw "Missing!"; }
+
 // CHECK-FIXES: {{\[\[}}clang::annotate("throws_exception"){{\]\]}} void missingExceptionAttributeInCatch() {
 void missingExceptionAttributeInCatch() {
   try {
     // . . .
   } catch (...) {
+    // CHECK-MESSAGES: :[[@LINE+1]]:5: warning: Function 'missingExceptionAttributeInCatch' can throw an exception but is not annotated as such. [fault-line-missing-exception-attribute]
     throw "Missing!";
   }
 }
 
+// CHECK-MESSAGES: :[[@LINE+1]]:22: warning: Lambda can throw an exception but is not annotated as such. [fault-line-missing-exception-attribute]
+auto lambda = []() { throw "Missing!"; };
+
 class TestClass {
-  // CHECK-MESSAGES: :[[@LINE+2]]:3: warning: Function 'TestClass' can throw an exception but is not annotated as such. [fault-line-missing-exception-attribute]
+  // CHECK-MESSAGES: :[[@LINE+2]]:17: warning: Constructor 'TestClass' can throw an exception but is not annotated as such. [fault-line-missing-exception-attribute]
   // CHECK-FIXES: {{\[\[}}clang::annotate("throws_exception"){{\]\]}} TestClass() { throw "Missing!"; }
   TestClass() { throw "Missing!"; }
 
-  // CHECK-MESSAGES: :[[@LINE+2]]:3: warning: Function '~TestClass' can throw an exception but is not annotated as such. [fault-line-missing-exception-attribute]
+  // CHECK-MESSAGES: :[[@LINE+2]]:18: warning: Destructor '~TestClass' can throw an exception but is not annotated as such. [fault-line-missing-exception-attribute]
   // CHECK-FIXES: {{\[\[}}clang::annotate("throws_exception"){{\]\]}} ~TestClass() { throw "Missing!"; }
   ~TestClass() { throw "Missing!"; }
 
-  // CHECK-MESSAGES: :[[@LINE+2]]:8: warning: Function 'missingExceptionAttribute' can throw an exception but is not annotated as such. [fault-line-missing-exception-attribute]
+  // CHECK-MESSAGES: :[[@LINE+2]]:38: warning: Method 'missingExceptionAttribute' can throw an exception but is not annotated as such. [fault-line-missing-exception-attribute]
   // CHECK-FIXES: {{\[\[}}clang::annotate("throws_exception"){{\]\]}} void missingExceptionAttribute() { throw "Missing!"; }
   void missingExceptionAttribute() { throw "Missing!"; }
 
-  // CHECK-MESSAGES: :[[@LINE+2]]:15: warning: Function 'staticMissingExceptionAttribute' can throw an exception but is not annotated as such. [fault-line-missing-exception-attribute]
+  // CHECK-MESSAGES: :[[@LINE+2]]:51: warning: Method 'staticMissingExceptionAttribute' can throw an exception but is not annotated as such. [fault-line-missing-exception-attribute]
   // CHECK-FIXES: {{\[\[}}clang::annotate("throws_exception"){{\]\]}} static void staticMissingExceptionAttribute() { throw "Missing!"; }
   static void staticMissingExceptionAttribute() { throw "Missing!"; }
 };
