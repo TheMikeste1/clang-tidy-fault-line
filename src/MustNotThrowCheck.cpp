93b28eb0eb1ee82fb129057c26333216bccbb457
Michael Hegerhorst <michael.hegerhorst@gmail.com>
2026-08-05 21:26:35 -0600

    Support IIFEs in MustNotThrow

--- src/MustNotThrowCheck.cpp
+++ src/MustNotThrowCheck.cpp
@@ -5,6 +5,7 @@
 #include <clang/AST/Attr.h>
 #include <clang/AST/Decl.h>
 #include <clang/AST/Expr.h>
+#include <clang/AST/ExprCXX.h>
 #include <clang/Basic/AttrKinds.h>
 #include <clang/Basic/DiagnosticIDs.h>
 #include <clang/Basic/LLVM.h>
@@ -16,39 +17,36 @@ namespace clang::tidy::fault_line {
 void MustNotThrowCheck::registerMatchers(MatchFinder *Finder) {
   // clang-format off
   Finder->addMatcher(
-    // cxxDestructorDecl(
-    //   hasDescendant(
-        callExpr(
-          anyOf(
-            callee(functionDecl(hasAttr(attr::Annotate)).bind("callee_decl")),
-            hasDescendant(declRefExpr(to(varDecl(hasAttr(attr::Annotate)).bind("callee_decl")))),
-            hasDescendant(memberExpr(hasDeclaration(fieldDecl(hasAttr(attr::Annotate)).bind("callee_decl"))))
-          ),
-          unless(hasAncestor(compoundStmt(hasParent(cxxTryStmt())))),
-          forCallable(functionDecl(hasType(functionProtoType(isNoThrow()))))
-        ).bind("unhandled_call")
-    //   )
-    // )
-
-    ,
+    callExpr(
+      anyOf(
+        callee(functionDecl(hasAttr(attr::Annotate)).bind("callee_decl")),
+        hasDescendant(declRefExpr(to(varDecl(hasAttr(attr::Annotate)).bind("callee_decl")))),
+        hasDescendant(memberExpr(hasDeclaration(fieldDecl(hasAttr(attr::Annotate)).bind("callee_decl")))),
+        hasDescendant(cxxThrowExpr(unless(hasAncestor(compoundStmt(hasParent(cxxTryStmt()))))).bind("throw_expr"))
+      ),
+      unless(hasAncestor(compoundStmt(hasParent(cxxTryStmt())))),
+      forCallable(functionDecl(hasType(functionProtoType(isNoThrow()))))
+    ).bind("unhandled_call"),
   this);
   // clang-format on
 }
 
 void MustNotThrowCheck::check(const MatchFinder::MatchResult &Result) {
   const auto *MatchedCall = Result.Nodes.getNodeAs<CallExpr>("unhandled_call");
-  const auto *CalleeDecl = Result.Nodes.getNodeAs<DeclaratorDecl>("callee_decl");
-  if (MatchedCall == nullptr || CalleeDecl == nullptr) {
+  if (MatchedCall == nullptr) {
     return;
   }
 
-  if (!hasThrowsExceptionAnnotation(*CalleeDecl)) {
+  if (const auto *CalleeDecl = Result.Nodes.getNodeAs<DeclaratorDecl>("callee_decl"); CalleeDecl != nullptr) {
+    if (hasThrowsExceptionAnnotation(*CalleeDecl)) {
+      diag(MatchedCall->getBeginLoc(), "Calling throwing function %0 without handling exceptions in a function that must not throw!", DiagnosticIDs::Level::Error) << CalleeDecl;
+    }
     return;
   }
 
-  diag(MatchedCall->getBeginLoc(),
-       "calling throwing function %0 without handling exceptions in a function that must not throw!",
-       DiagnosticIDs::Level::Error)
-      << CalleeDecl;
+  if (const auto *ThrowExpr = Result.Nodes.getNodeAs<CXXThrowExpr>("throw_expr"); ThrowExpr != nullptr) {
+    diag(ThrowExpr->getBeginLoc(), "Calling IIFE without handling exceptions in a function that must not throw!", DiagnosticIDs::Level::Error);
+    return;
+  }
 }
 } // namespace clang::tidy::fault_line
--- test/test_must_not_throw_exception.cpp
+++ test/test_must_not_throw_exception.cpp
@@ -1,19 +1,24 @@
 [[clang::annotate("throws_exception")]] bool hasExceptionAttribute();
 [[clang::annotate("throws_exception")]] auto lambda = []() { throw "Exception!"; };
 
-// CHECK-MESSAGES: :[[@LINE+1]]:36: error: calling throwing function 'hasExceptionAttribute' without handling exceptions in a function that must not throw! [fault-line-must-not-throw]
+// CHECK-MESSAGES: :[[@LINE+1]]:36: error: Calling throwing function 'hasExceptionAttribute' without handling exceptions in a function that must not throw! [fault-line-must-not-throw]
 void noexceptFunction() noexcept { hasExceptionAttribute(); }
 
 class TestClass {
-  // CHECK-MESSAGES: :[[@LINE+1]]:18: error: calling throwing function 'hasExceptionAttribute' without handling exceptions in a function that must not throw! [fault-line-must-not-throw]
+  // CHECK-MESSAGES: :[[@LINE+1]]:18: error: Calling throwing function 'hasExceptionAttribute' without handling exceptions in a function that must not throw! [fault-line-must-not-throw]
   ~TestClass() { hasExceptionAttribute(); }
 
-  // CHECK-MESSAGES: :[[@LINE+1]]:38: error: calling throwing function 'hasExceptionAttribute' without handling exceptions in a function that must not throw! [fault-line-must-not-throw]
+  // CHECK-MESSAGES: :[[@LINE+1]]:38: error: Calling throwing function 'hasExceptionAttribute' without handling exceptions in a function that must not throw! [fault-line-must-not-throw]
   void noexceptFunction() noexcept { hasExceptionAttribute(); }
 
-  // CHECK-MESSAGES: :[[@LINE+1]]:51: error: calling throwing function 'hasExceptionAttribute' without handling exceptions in a function that must not throw! [fault-line-must-not-throw]
+  // CHECK-MESSAGES: :[[@LINE+1]]:51: error: Calling throwing function 'hasExceptionAttribute' without handling exceptions in a function that must not throw! [fault-line-must-not-throw]
   static void staticNoexceptFunction() noexcept { hasExceptionAttribute(); }
 
-  // CHECK-MESSAGES: :[[@LINE+1]]:46: error: calling throwing function 'lambda' without handling exceptions in a function that must not throw! [fault-line-must-not-throw]
+  // CHECK-MESSAGES: :[[@LINE+1]]:46: error: Calling throwing function 'lambda' without handling exceptions in a function that must not throw! [fault-line-must-not-throw]
   void noexceptCallLambda() noexcept { lambda(); }
+
+  void noexceptIife() noexcept {
+  // CHECK-MESSAGES: :[[@LINE+1]]:12: error: Calling IIFE without handling exceptions in a function that must not throw! [fault-line-must-not-throw]
+    []() { throw "Exception!"; }();
+  }
 };
